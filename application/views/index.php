79a21b2906b2eca99f54cc938adde964978451e4
wesllen <wesllenalves@gmail.com>
2019-04-16 17:22:23 -0300

    implementando a separação do head e do footer na pasta template e chamando elas no controller

--- application/views/index.php
+++ application/views/index.php
@@ -1,25 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Bootstrap User Management Data Table</title>
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
-<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-<link rel="stylesheet" href="<?php echo base_url('assets/node_modules/@fortawesome/fontawesome-free/css/fontawesome.min.css')?>"><!--"https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"-->
-<link rel="stylesheet" href="<?php echo base_url('assets/node_modules/bootstrap/dist/css/bootstrap.min.css')?>"><!--https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css-->
-<link rel="stylesheet" href="<?php echo base_url('assets/css/custom.css')?>"></script>
-<script src="<?php echo base_url('assets/node_modules/jquery/dist/jquery.min.js')?>"></script><!--"https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"-->
-<script src="<?php echo base_url('assets/node_modules/bootstrap/dist/js/bootstrap.min.js')?>"></script><!--"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"-->
-
-<script type="text/javascript">
-$(document).ready(function(){
-	$('[data-toggle="tooltip"]').tooltip();
-});
-</script>
-</head>
-<body>
     <div class="container">
         <div class="table-wrapper">
             <div class="table-title">
@@ -115,5 +93,4 @@ $(document).ready(function(){
             </div>
         </div>
     </div>     
-</body>
-</html>  
+
